442d2539779575aa68a1507b742b59dfaf9c0aaf
Md. Jafrin Hossain <md.jafrin.hossain@g.bracu.ac.bd>
2023-02-22 01:15:03 +0600

    adding breeze'

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <h1>About page from blade</h1>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <h1>Contact page from blade</h1>
+</body>
+</html>
